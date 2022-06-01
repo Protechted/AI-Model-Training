c4eb127601a79b26b5ff5cc1d45d2bc6f5ca24fb
Daniel Ebert <daniel.ebert@ibm.com>
2022-06-01 14:56:18 +0200

    Added notify for accelerometer, values still need to be converted

--- capturecharacteristics.py
+++ capturecharacteristics.py
@@ -3,6 +3,11 @@ import sys
 from bleak import BleakClient
 
 temperature_uuid = "19b10000-2001-537e-4f6c-d104768a1214"
+accelerometer_uuid = '19b10000-5001-537e-4f6c-d104768a1214'
+
+def accelerometer_data_callback(handle, data):
+  print(handle, data)
+
 
 async def main(address):
   async with BleakClient(address) as client:
@@ -10,9 +15,12 @@ async def main(address):
       raise "client not connected"
 
     services = await client.get_services()
+    await client.start_notify(accelerometer_uuid, accelerometer_data_callback)
 
     temp_bytes = await client.read_gatt_char(temperature_uuid)
     print( temp_bytes)
+    await asyncio.sleep(60)
+    await client.stop_notify(accelerometer_uuid)
 
 
 if __name__ == "__main__":
