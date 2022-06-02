7a2d0ce6fee2fd57a969b13c39773e3bcfc7ca56
Daniel Ebert <daniel.ebert@ibm.com>
2022-06-02 19:06:53 +0200

    All generally neccessary values from humidity, temperature, accelerometer,gyroscope and quaternation sensor can be retrieved

--- capturecharacteristics.py
+++ capturecharacteristics.py
@@ -1,44 +1,67 @@
 import asyncio
 import sys
+import struct
 from bleak import BleakClient
+from binascii import hexlify
 
-temperature_uuid = "19b10000-2001-537e-4f6c-d104768a1214"
-accelerometer_uuid = '19b10000-5001-537e-4f6c-d104768a1214'
+temperature_uuid = "19b10000-2001-537e-4f6c-d104768a1214" # 4 Byte float32
+humidity_uuid = "19b10000-3001-537e-4f6c-d104768a1214" # 1 byte Uint8
+accelerometer_uuid = "19b10000-5001-537e-4f6c-d104768a1214" # 3 times 4 byte float32 in an array
+gyroscope_uuid = "19b10000-6001-537e-4f6c-d104768a1214" # 3 times 2 byte Uint16 in an array
+quaternion_uuid = "19b10000-7001-537e-4f6c-d104768a1214" # 4 times 4 byte float32 in an array
+
+
+def gyroscope_data_callback(handle, data):
+    # print(handle, data)
+    [x,y,z] = struct.unpack('HHH', data)
+    #print(x)
+
+    pass
+def quaternation_data_callback(handle, data):
+    # print(handle, data)
+    [x,y,z,w] = struct.unpack('ffff', data)
+    print(x)
 
 def accelerometer_data_callback(handle, data):
-  #print(handle, data)
-  pass
+    # print(handle, data)
+    [Ax,Ay,Az] = struct.unpack('fff', data)
+    #print(Ax)
 
 
 async def main(address):
-  async with BleakClient(address) as client:
-    if (not client.is_connected):
-      raise "client not connected"
+    async with BleakClient(address) as client:
+        if (not client.is_connected):
+            raise "client not connected"
 
-    services = await client.get_services()
-    await client.start_notify(accelerometer_uuid, accelerometer_data_callback)
-
-    temp_bytes = await client.read_gatt_char(temperature_uuid)
-    convert_temp_bytes_to_float(temp_bytes)
-
-    await asyncio.sleep(60)
-    await client.stop_notify(accelerometer_uuid)
+        services = await client.get_services()
+        await client.start_notify(accelerometer_uuid, accelerometer_data_callback)
+        await client.start_notify(gyroscope_uuid, gyroscope_data_callback)
+        await client.start_notify(quaternion_uuid, quaternation_data_callback)
 
 
-def convert_temp_bytes_to_float(temp_bytes):
-    temp_bytes = byte_array_to_int(temp_bytes)
-    print( temp_bytes)
+
+        temp_bytes = await client.read_gatt_char(temperature_uuid)         #print(hexlify(temp_bytes))
+        [temperature] = struct.unpack('f', temp_bytes)
+        print(temperature)
+
+        humidity_bytes = await client.read_gatt_char(humidity_uuid)
+        [humidity] = struct.unpack('i', humidity_bytes)
+        print(humidity)
+
+
+        await asyncio.sleep(6000000)
+        await client.stop_notify(accelerometer_uuid)
+
+
+def indef_loop():
+    while (True):
+        pass
 
 def byte_array_to_int(value):
-    # Raw data is hexstring of int values, as a series of bytes, in little endian byte order
-    # values are converted from bytes -> bytearray -> int
-    # e.g., b'\xb8\x08\x00\x00' -> bytearray(b'\xb8\x08\x00\x00') -> 2232
-
-    # print(f"{sys._getframe().f_code.co_name}: {value}")
-
     value = bytearray(value)
-    value = int.from_bytes(value, byteorder="big")
+    value = int.from_bytes(value, byteorder="little")
     return value
+
 
 if __name__ == "__main__":
     address = "42D1EB68-5EDF-85F9-D05E-82E0AD1CBD94"
