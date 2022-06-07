652e5a9c47fbbd0c519ccad8660dc68aa7adca6f
Daniel <web.par@t-online.de>
2022-06-07 11:25:08 +0200

    UI updates

--- dashboard/plotly_dash_start.py
+++ dashboard/plotly_dash_start.py
@@ -8,15 +8,17 @@ from matplotlib import pyplot as plt
 
 import dashsubcomponents
 from dash import Dash
+import dash
 from dash.dependencies import Input, Output
 from dash_extensions import WebSocket
+from dash_extensions.enrich import Output, DashProxy, Input, MultiplexerTransform
 import dash_bootstrap_components as dbc
-from dash import Output, html, dcc, Input
+from dash import Output, html, dcc, Input, ctx
 import pandas as pd
 import plotly.tools as tls
 
 # Create example app.
-app = Dash(__name__, prevent_initial_callbacks=True, external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = DashProxy(prevent_initial_callbacks=True, transforms=[MultiplexerTransform()], external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 CONTENT_STYLE = {
     "margin-left": "18rem",
@@ -26,29 +28,40 @@ CONTENT_STYLE = {
 
 websocket = html.Div([
     dcc.Input(id="input", autoComplete="off"), html.Div(id="message"),
-
 ])
 
-
-mltraining = html.Div([html.P("""Commands: "start" to start the Training. Results will be plotted when the training finished. """), dbc.Button('Start training', id='starttraining', n_clicks=0), websocket])
+mltraining = html.Div(
+    [html.P("""Commands: "start" to start the Training. Results will be plotted when the training finished. """),
+     dbc.Button('Start training', id='starttraining', n_clicks=0),html.Div(style={"margin-top": "5px"}), websocket])
 mainpage = html.Div([html.P("Willkommen"), websocket])
-liveData = html.Div([html.P("""Live Data from the sensor. Commands: "live" to start the live Transmitting, "stopLive" to stop it. """), websocket, html.Br(), html.H2("Daten Beschleunigungssensor"), dcc.Graph(id = 'live-graph-accelerometer', animate = False, config={"responsive":True})])
-
+liveData = html.Div(
+    [html.P("""Live Data from the sensor. Commands: "live" to start the live Transmitting, "stopLive" to stop it. """),
+     html.Div(id="hidden_div_for_redirect_callback"),
+     dbc.Button('Start live transmit', id='startLiveTransmit', n_clicks=0, style={"margin-right": "4px"}),dbc.Button('Stop live transmit', id='stopLiveTransmit', n_clicks=0),html.Div(style={"margin-top": "5px"}), websocket, html.Br(),
+     html.H2("Daten Beschleunigungssensor"),
+     dcc.Graph(id='live-graph-accelerometer', animate=False, config={"responsive": True})])
 
 content = html.Div(id="page-content", style=CONTENT_STYLE)
-app.layout = html.Div([dcc.Location(id="url"), dashsubcomponents.sidebar, content, WebSocket(url="ws://127.0.0.1:5300/", id="ws")])
+app.layout = html.Div(
+    [dcc.Location(id="url"), dashsubcomponents.sidebar, content, WebSocket(url="ws://192.168.8.218:5300/", id="ws")])
 
+@app.callback(Output("ws", "send"), Output("hidden_div_for_redirect_callback", "children"), Input('startLiveTransmit', 'n_clicks'), prevent_initial_call=True)
+def send(n_clicks):
+    return "live", dcc.Location(pathname="/livedata", id="someid_doesnt_matter")
 
-@app.callback(
-    Output("input", "value"),
-    Input('starttraining','n_clicks'),
-)
-def startTrainingButton():
+@app.callback(Output("ws", "send"), Input('stopLiveTransmit', 'n_clicks'), prevent_initial_call=True)
+def send(n_clicks):
+    return "stopLive"
+
+@app.callback(Output("ws", "send"), Input('starttraining', 'n_clicks'), prevent_initial_call=True)
+def send(n_clicks):
+    print("Test")
     return "start"
 
-@app.callback(Output("ws", "send"), [Input("input", "value")])
+@app.callback(Output("ws", "send"), Input("input", "value"), prevent_initial_call=True)
 def send(value):
     return value
+
 
 @app.callback(Output('page-content', 'children'),
               [Input('url', 'pathname')])
@@ -62,24 +75,28 @@ def display_page(pathname):
     else:
         return mainpage
 
+
 def df_to_plotly(df):
     return {'z': df.values.tolist(),
             'x': df.columns.tolist(),
             'y': df.columns.tolist()}
 
+
 @app.callback(Output("message", "children"), [Input("ws", "message")])
 def message(e):
-    if (str(e['data']).startswith("dataframeoutput:")):
+    if str(e['data']) == None:
+        return
+    if str(e['data']).startswith("dataframeoutput:"):
         dataframe_string = str(e['data']).split("dataframeoutput:")[1]
         loaded_df = pd.read_json(dataframe_string, orient='index')
 
         print(loaded_df)
         print(loaded_df['ax'].tolist())
 
-        #Correlation logic
+        # Correlation logic
         corr = loaded_df.corr()
         fig = go.Figure(data=go.Heatmap(df_to_plotly(loaded_df)))
-        #End correlation logic
+        # End correlation logic
 
         return html.Div([dcc.Graph(
             id='ResultGraph',
@@ -95,36 +112,43 @@ def message(e):
                 }
             }
         ),
-        dcc.Graph(
-            id='ResultGraphGyroscope',
-            figure={
-                'data': [
-                    {"x": loaded_df.index.values.tolist(), "y": loaded_df['gx'].tolist(), 'type': 'line', 'name': 'Gx'},
-                    {"x": loaded_df.index.values.tolist(), "y": loaded_df['gy'].tolist(), 'type': 'line', 'name': 'Gy'},
-                    {"x": loaded_df.index.values.tolist(), "y": loaded_df['gz'].tolist(), 'type': 'line', 'name': 'Gz'},
-                ],
-                'layout': {
-                    'title': 'Recorded Data Graph - Gyroscope',
-                    'showlegend': 'true'
+            dcc.Graph(
+                id='ResultGraphGyroscope',
+                figure={
+                    'data': [
+                        {"x": loaded_df.index.values.tolist(), "y": loaded_df['gx'].tolist(), 'type': 'line',
+                         'name': 'Gx'},
+                        {"x": loaded_df.index.values.tolist(), "y": loaded_df['gy'].tolist(), 'type': 'line',
+                         'name': 'Gy'},
+                        {"x": loaded_df.index.values.tolist(), "y": loaded_df['gz'].tolist(), 'type': 'line',
+                         'name': 'Gz'},
+                    ],
+                    'layout': {
+                        'title': 'Recorded Data Graph - Gyroscope',
+                        'showlegend': 'true'
+                    }
                 }
-            }
-        ),
-        dcc.Graph(
-            id='ResultGraphMagnetometer',
-            figure={
-                'data': [
-                    {"x": loaded_df.index.values.tolist(), "y": loaded_df['qx'].tolist(), 'type': 'line', 'name': 'qx'},
-                    {"x": loaded_df.index.values.tolist(), "y": loaded_df['qy'].tolist(), 'type': 'line', 'name': 'qy'},
-                    {"x": loaded_df.index.values.tolist(), "y": loaded_df['qz'].tolist(), 'type': 'line', 'name': 'qz'},
-                    {"x": loaded_df.index.values.tolist(), "y": loaded_df['qw'].tolist(), 'type': 'line', 'name': 'qw'},
+            ),
+            dcc.Graph(
+                id='ResultGraphMagnetometer',
+                figure={
+                    'data': [
+                        {"x": loaded_df.index.values.tolist(), "y": loaded_df['qx'].tolist(), 'type': 'line',
+                         'name': 'qx'},
+                        {"x": loaded_df.index.values.tolist(), "y": loaded_df['qy'].tolist(), 'type': 'line',
+                         'name': 'qy'},
+                        {"x": loaded_df.index.values.tolist(), "y": loaded_df['qz'].tolist(), 'type': 'line',
+                         'name': 'qz'},
+                        {"x": loaded_df.index.values.tolist(), "y": loaded_df['qw'].tolist(), 'type': 'line',
+                         'name': 'qw'},
 
-                ],
-                'layout': {
-                    'title': 'Recorded Data Graph - Magnetometer',
-                    'showlegend': 'true'
+                    ],
+                    'layout': {
+                        'title': 'Recorded Data Graph - Magnetometer',
+                        'showlegend': 'true'
+                    }
                 }
-            }
-        ),
+            ),
             dcc.Graph(
                 id='ResultGraphPressure',
                 figure={
@@ -148,11 +172,11 @@ def message(e):
     return f"Response from websocket: {e['data']}"
 
 
-X = deque(maxlen = 50)
+X = deque(maxlen=50)
 xaxiscounter = 0
-YforAx = deque(maxlen = 50)
-YforAy = deque(maxlen = 50)
-YforAz = deque(maxlen = 50)
+YforAx = deque(maxlen=50)
+YforAy = deque(maxlen=50)
+YforAz = deque(maxlen=50)
 
 
 @app.callback(Output("live-graph-accelerometer", "figure"), [Input("ws", "message")])
@@ -162,10 +186,12 @@ def message(e):
     global YforAx
     global YforAy
     global YforAz
+    if str(e['data']) == None:
+        return
     if (str(e['data']).startswith("liveData:")):
         dict_string = str(e['data']).split("liveData:")[1]
         dict_values = json.loads(dict_string)
-        xaxiscounter +=1
+        xaxiscounter += 1
         X.append(xaxiscounter)
         YforAx.append(dict_values["ax"])
         YforAy.append(dict_values["ay"])
@@ -181,23 +207,24 @@ def message(e):
             }
         }
         dataAx = plotly.graph_objs.Scatter(
-                x=list(X),
-                y=list(YforAx),
-                name='AccelerometerX',
+            x=list(X),
+            y=list(YforAx),
+            name='AccelerometerX',
         )
         dataAy = plotly.graph_objs.Scatter(
-                x=list(X),
-                y=list(YforAy),
-                name='AccelerometerY',
+            x=list(X),
+            y=list(YforAy),
+            name='AccelerometerY',
         )
         dataAz = plotly.graph_objs.Scatter(
-                x=list(X),
-                y=list(YforAz),
-                name='AccelerometerZ',
+            x=list(X),
+            y=list(YforAz),
+            name='AccelerometerZ',
         )
 
-        return {'data': [dataAx,dataAy,dataAz],
-                'layout': go.Layout(xaxis=dict(range=[min(X), max(X)]), uirevision = True)}
+        return {'data': [dataAx, dataAy, dataAz],
+                'layout': go.Layout(xaxis=dict(range=[min(X), max(X)]), uirevision=True)}
+
 
 if __name__ == '__main__':
     app.run_server()
