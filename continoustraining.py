c11f94f992d8fd6383541a5af070eacddf0794eb
Ben Schaper <ben.schaper@ibm.com>
2022-06-10 13:42:04 +0200

    add insert for contiuous training

--- continoustraining.py
+++ continoustraining.py
@@ -1,16 +1,27 @@
 import asyncio
+from threading import Thread
 import websockets
 import pandas as pd
 from dashboard.insert import insert_data
+import signal
+import sys
 
+stop_bool: bool = True
+
+def signal_handler(sig, frame):
+    global stop_bool
+    stop_bool = False
+    print('You pressed Ctrl+C!')
 
 async def run_websocket():
+    global stop_bool
+
     async with websockets.connect(
             'ws://localhost:5300') as websocket:
         await websocket.send("startcontinoustraining")
         print("Sent Continous Training request to websocket server")
 
-        while True:
+        while stop_bool:
             answer = await websocket.recv()
             print("Received from websocket: "+ answer)
             if str(answer) == None:
@@ -18,8 +29,10 @@ async def run_websocket():
             if str(answer).startswith("dataframeoutputcontinous:"):
                 dataframe_string = str(answer).split("dataframeoutputcontinous:")[1]
                 loaded_df = pd.read_json(dataframe_string, orient='index')
-                print(loaded_df)
-
+                thread = Thread(target=insert_data, args=(loaded_df, "D4", "ContinousTraining_Ben"))
+                thread.start()
+                print("Inserted Continous Training Data")
 
 if __name__ == "__main__":
+    signal.signal(signal.SIGINT, signal_handler)
     asyncio.get_event_loop().run_until_complete(run_websocket())
