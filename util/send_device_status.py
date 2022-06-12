327d49c9dff0ac50032623352ad5e077f5dda4dc
Daniel Ebert <daniel.ebert@ibm.com>
2022-06-12 16:58:19 +0200

    Additions to Device Heartbeat

--- util/send_device_status.py
+++ util/send_device_status.py
@@ -1,4 +1,7 @@
 import struct
+from threading import Thread
+import requests
+import json
 
 temperature_uuid = "19b10000-2001-537e-4f6c-d104768a1214"  # 4 Byte float32
 humidity_uuid = "19b10000-3001-537e-4f6c-d104768a1214"  # 1 byte Uint8
@@ -13,8 +16,24 @@ async def execute_device_heartbeat(client):
     humidity_bytes = await client.read_gatt_char(humidity_uuid)
     [humidity] = struct.unpack('I', humidity_bytes)
     print("Humidity at the Moment:" + str(humidity))
+    #execute_request_thread = Thread(target=send_device_heartbeat_request,
+    #                                args=(temperature, humidity))
+    #execute_request_thread.start()
 
 
 def send_device_heartbeat_request(temperature, humidity):
 
-    pass
+
+    url = "https://europe-west1-erudite-visitor-336410.cloudfunctions.net/updateDeviceState"
+
+    payload = json.dumps({
+        "deviceId": "did__1",
+        "batteryLevel": 100
+    })
+    headers = {
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    print(response.text)
