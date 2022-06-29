6525abc62a09c99e984936b1b1cba1371d519d78
Daniel Ebert <daniel.ebert@ibm.com>
2022-06-29 17:58:12 +0200

    Static Quaternion Hotfix

--- capturecharacteristics.py
+++ capturecharacteristics.py
@@ -244,7 +244,7 @@ async def handler(websocket):
 
 if __name__ == "__main__":
     #address = "02D307CC-39AB-9D1B-A279-6B8245193D28"
-    #address = "42D1EB68-5EDF-85F9-D05E-82E0AD1CBD94" # Daniel Nicla Sense
-    address = "5715B4EA-BFAD-A3E6-04AE-0558170BA098"
+    address = "42D1EB68-5EDF-85F9-D05E-82E0AD1CBD94" # Daniel Nicla Sense
+    #address = "5715B4EA-BFAD-A3E6-04AE-0558170BA098"
     print('address:', address)
     asyncio.run(main(address))
